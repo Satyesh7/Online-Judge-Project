e2a2385f5cd37219491151db3a25933a48837c2f
Satyesh Das <satyeshdas7@gmail.com>
2024-08-29 17:13:23 +0530

    Reset Git tracking based on new .gitignore

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Signin from "./pages/Signin"
 import Signup from "./pages/Signup"
 import Home from "./pages/Home"
 import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 
 export default function App(){
@@ -16,6 +17,7 @@ export default function App(){
       <Route path="/sign-in" element={<Signin/>}/>
       <Route path="/sign-up" element={<Signup/>}/>
      </Routes>
+     <Footer/>
     </BrowserRouter>
   )
 }
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,85 @@
+import React from 'react'
+import { Footer } from 'flowbite-react'
+import { Link } from 'react-router-dom';
+import {BsFacebook, BsGithub, BsTwitter, BsInstagram} from 'react-icons/bs' 
+
+export default function Footercom() {
+  return( <Footer container className='border border-t-8 border-teal-500'>
+    <div className='w-full max-w-7xl mx-auto'>
+        <div className='grid w-full justify-between sm:flex md:grid-cols-1'>
+            <div className='mt-5'>
+            <Link to="/" className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'>
+        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white' > Online Judge
+        </span> 
+        </Link>
+            </div>
+            <div className='grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6'>
+                <div>
+                <Footer.Title title='About'/>
+                <Footer.LinkGroup col>
+                    <Footer.Link 
+                    href='https://www.linkedin.com/in/satyesh-das-972791228/'
+                    target='_blank'
+                    rel='noopener noreferrer'>
+                        LinkedIn
+                    </Footer.Link>
+                    <Footer.Link 
+                    href='/about'
+                    target='_blank'
+                    rel='noopener noreferrer'>
+                        Features
+                    </Footer.Link>
+                </Footer.LinkGroup>
+                </div>
+                <div>
+                <Footer.Title title='Social Media Handles'/>
+                <Footer.LinkGroup col>
+                    <Footer.Link 
+                    href='https://www.instagram.com/satyeshdas7/?hl=en'
+                    target='_blank'
+                    rel='noopener noreferrer'>
+                        Instagram
+                    </Footer.Link>
+                    <Footer.Link 
+                    href='https://github.com/Satyesh7'
+                    target='_blank'
+                    rel='noopener noreferrer'>
+                        Github
+                    </Footer.Link>
+                </Footer.LinkGroup>
+                </div>
+                <div>
+                <Footer.Title title='Legal'/>
+                <Footer.LinkGroup col>
+                    <Footer.Link 
+                    href='#'>
+                        Privacy Policy
+                    </Footer.Link>
+                    <Footer.Link 
+                    href='#'>
+                        Terms &amp; Conditions
+                    </Footer.Link>
+                </Footer.LinkGroup>
+                </div>
+                
+
+            </div>
+
+        </div>
+        <Footer.Divider />
+            <div className='w-full sm:flex sm:items-center sm:justify-between'>
+                <Footer.Copyright href='#' 
+                by="Online Judge"
+                year={new Date().getFullYear()}/>
+                <div className='flex gap-6 sm:mt-0 mt-4 sm:justify-center'>
+                    <Footer.Icon href='#' icon={BsFacebook}/>
+                    <Footer.Icon href='https://www.instagram.com/satyeshdas7/?hl=en' icon={BsInstagram}/>
+                    <Footer.Icon href='https://x.com/home' icon={BsTwitter}/>
+                    <Footer.Icon href='https://github.com/Satyesh7' icon={BsGithub}/>
+                </div>
+
+                
+            </div>
+    </div>
+    </Footer> );
+}
