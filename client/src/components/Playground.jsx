5c7ae21036c2968ece2d8ae19c148df62c88fc60
Satyesh Das <satyeshdas7@gmail.com>
2024-09-24 20:53:12 +0530

    Add testcases UI and add EditorFooter

--- client/src/components/Playground.jsx
+++ client/src/components/Playground.jsx
@@ -4,16 +4,20 @@ import Split from "react-split";
 import { javascript } from "@codemirror/lang-javascript";
 import CodeMirror from "@uiw/react-codemirror";
 import { vscodeDark } from "@uiw/codemirror-theme-vscode";
+import EditorFooter from "./EditorFooter";
 
 const Playground = () => {
+    const boilerPlate = `function twoSum(nums, target) {
+    //Write your code here
+    };`;
   return (
-    <div className='flex flex-col bg-dark-layer-1 relative '>
+    <div className='flex flex-col bg-dark-layer-1 relative overflow-x-hidden'>
         <PreferenceNav  />
 
         <Split className='h-[calc(100vh-94px)]' direction='vertical' sizes={[60, 40]} minSize={60}>
             <div className='w-full overflow-auto'>
                 <CodeMirror
-                    value='const a = 1;'
+                    value={boilerPlate}
                     theme={vscodeDark}
                   
                     extensions={[javascript()]}
@@ -29,9 +33,64 @@ const Playground = () => {
                     </div>
                 </div>
 
-    
+                <div className='flex'>
+
+{/* case 1*/}
+
+<div className='mr-2 items-start mt-2text-white'>
+<div className='flex flex-wrap items-center gap-y-4'>
+<div className='font-medium items-center transition-all focus: outline-none inline-flex bg-dark-fill-3
+hover:bg-dark-fill-2 relative rounded-1g px-4 py-1 cursor-pointer whitespace-nowrap'>
+
+Case 1
+
+</div>
+</div>
+</div>
+
+{/* case 2*/}
+
+<div className='mr-2 items-start mt-2text-white'>
+<div className='flex flex-wrap items-center gap-y-4'>
+<div className='font-medium items-center transition-all focus: outline-none inline-flex bg-dark-fill-3
+hover:bg-dark-fill-2 relative rounded-1g px-4 py-1 cursor-pointer whitespace-nowrap'>
+
+Case 2
+
+</div>
+</div>
+</div>
+
+{/* case 3*/}
+
+<div className='mr-2 items-start mt-2text-white'>
+<div className='flex flex-wrap items-center gap-y-4'>
+<div className='font-medium items-center transition-all focus: outline-none inline-flex bg-dark-fill-3
+hover:bg-dark-fill-2 relative rounded-1g px-4 py-1 cursor-pointer whitespace-nowrap'>
+
+Case 3
+
+</div>
+</div>
+</div>
+</div>
+
+<div className="font-semibold my-4">
+<p className='text-sm font-medium mt-4 text-white'>Input:</p>
+<div className="w-full cursor-text rounded-1g border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2">
+
+nums: [2,7,11,15], target: 9
+</div>
+<p className="text-sm font-medium mt-4 text-white">Output:</p> 
+<div classiame="w-full cursor-text rounded-1g border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2">
+
+[0,1]
+
+</div>
+</div>
             </div>
         </Split>
+        <EditorFooter/>
     
     </div>
 );
